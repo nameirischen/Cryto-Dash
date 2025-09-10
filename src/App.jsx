9a3d6438c5d2677b8e7aeb705bda83a2f4b6903c
Iris Chen <nameirischen@gmail.com>
2025-09-10 14:29:45 +0800

    Crypto Dash

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import CoinCard from './Component/CoinCard';
+const API_URL =
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+
+const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL);
+        if (!res.ok) throw new Error('Failed to fetch data');
+        const data = await res.json();
+        setCoins(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoins();
+  }, []);
+
+  return (
+    <div>
+      <h1>🚀 Crypto Dash</h1>
+       {loading && <p>Loading...</p>}
+      {error && (
+        <div className='error'>
+          <p>❌ {error}</p>
+        </div>
+      )}
+
+      {!loading && !error && (
+        <main className='grid'>
+          {coins.map((coin) => (
+            <CoinCard key={coin.id} coin={coin}></CoinCard>
+          ))}
+        </main>
+      )}
+    </div>
+  );
+};
+
+export default App;
